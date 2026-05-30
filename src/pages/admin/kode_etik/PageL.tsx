3ebe9bdd71b24becc7bbf2f6702c7a05ea2333a8
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 19:06:45 +0700

    Update PageL.tsx

--- src/pages/admin/kode_etik/PageL.tsx
+++ src/pages/admin/kode_etik/PageL.tsx
@@ -15,7 +15,7 @@ export default function PageL() {
   });
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/api/Data_Unduh/${id}`).then((res) => {
+    axios.get(`${import.meta.env.VITE_API_URL}/Data_Unduh/${id}`).then((res) => {
       setForm(res.data);
     });
   }, [id]);
@@ -31,7 +31,7 @@ export default function PageL() {
       }
 
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/Data_Unduh",
+        `${import.meta.env.VITE_API_URL}/Data_Unduh`,
         formData,
         {
           headers: {
@@ -63,7 +63,7 @@ export default function PageL() {
         formData.append("file", file_path);
       }
       await axios.post(
-        `http://127.0.0.1:8000/api/Data_Unduh/${form.id}`,
+        `${import.meta.env.VITE_API_URL}/Data_Unduh/${form.id}`,
         formData,
         {
           headers: {
