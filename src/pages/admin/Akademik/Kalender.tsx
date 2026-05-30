613f4934c753c880a9d1656c73fed5c0e15ba2df
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:58:49 +0700

    Update Kalender.tsx

--- src/pages/admin/Akademik/Kalender.tsx
+++ src/pages/admin/Akademik/Kalender.tsx
@@ -149,7 +149,7 @@ export default function AKalender() {
                           "redo",
                         ],
                         ckfinder: {
-                          uploadUrl: "http://127.0.0.1:8000/api/upload",
+                          uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                         },
                       }}
                     />
