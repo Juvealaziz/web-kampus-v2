d0de1c82b9fcf782411a2f8f49c1eece68cea41f
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:42:25 +0700

    Update BKU.tsx

--- src/pages/admin/Biro/BKU.tsx
+++ src/pages/admin/Biro/BKU.tsx
@@ -149,7 +149,7 @@ export default function ABiroK() {
                           "redo",
                         ],
                         ckfinder: {
-                          uploadUrl: "http://127.0.0.1:8000/api/upload",
+                          uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                         },
                       }}
                     />
