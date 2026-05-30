5da010adc403f3084ae948457d486b204f69a80c
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:39:56 +0700

    Update PageL.tsx

--- src/pages/admin/Fakultas/PageL.tsx
+++ src/pages/admin/Fakultas/PageL.tsx
@@ -391,7 +391,7 @@ export default function PageL() {
                     "redo",
                   ],
                   ckfinder: {
-                    uploadUrl: "http://127.0.0.1:8000/api/upload",
+                    uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                   },
                 }}
               />
