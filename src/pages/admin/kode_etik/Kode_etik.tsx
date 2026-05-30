6e66af026150384c7bb4d4d05679372ddf3ca86f
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 19:06:03 +0700

    Update Kode_etik.tsx

--- src/pages/admin/kode_etik/Kode_etik.tsx
+++ src/pages/admin/kode_etik/Kode_etik.tsx
@@ -10,7 +10,7 @@ export default function AKode() {
   const navigate = useNavigate();
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/api/Data_Unduh")
+      .get(`${import.meta.env.VITE_API_URL}/Data_Unduh`)
       .then((res) => {
         const filtered = res.data.filter(
           (item: any) => item.tipe === "kode_etik",
@@ -28,7 +28,7 @@ export default function AKode() {
     if (confirm("Apakah kamu yakin ingin menghapus data ini?")) {
       try {
         const response = await axios.delete(
-          `http://127.0.0.1:8000/api/Data_Unduh/${id}`,
+          `${import.meta.env.VITE_API_URL}/Data_Unduh/${id}`,
         );
 
         alert(response.data.message);
