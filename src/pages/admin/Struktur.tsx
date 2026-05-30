939dd1024bd20d3e89ce71ced3e48e71df5416c2
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:24:18 +0700

    Update Struktur.tsx

--- src/pages/admin/Struktur.tsx
+++ src/pages/admin/Struktur.tsx
@@ -150,7 +150,7 @@ export default function AStruktur() {
                           "redo",
                         ],
                         ckfinder: {
-                          uploadUrl: "http://127.0.0.1:8000/api/upload",
+                          uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                         },
                       }}
                     />
