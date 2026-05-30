23b7f1c24eb0144800f49a051f2214d99c78ec00
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:33:19 +0700

    Update Aksi_Prestasi.tsx

--- src/pages/admin/Prestasi/Aksi_Prestasi.tsx
+++ src/pages/admin/Prestasi/Aksi_Prestasi.tsx
@@ -191,7 +191,7 @@ export default function AksiPrestasi() {
                             "redo",
                           ],
                           ckfinder: {
-                            uploadUrl: "http://127.0.0.1:8000/api/upload",
+                            uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                           },
                         }}
                       />
