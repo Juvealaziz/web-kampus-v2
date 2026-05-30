6ac809b2b945f3f65ca83624af44099a65ab129e
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:30:01 +0700

    Update EditLomba.tsx

--- src/pages/admin/lomba/EditLomba.tsx
+++ src/pages/admin/lomba/EditLomba.tsx
@@ -161,7 +161,7 @@ export default function EditLomba() {
                           "redo",
                         ],
                         ckfinder: {
-                          uploadUrl: "http://127.0.0.1:8000/api/upload",
+                          uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                         },
                       }}
                     />
