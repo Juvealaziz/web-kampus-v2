5ae2a9b4f1c85424cbe7545ac9c5f03a4d128ea8
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:34:59 +0700

    Update Sakip.tsx

--- src/pages/admin/Layanan/Sakip.tsx
+++ src/pages/admin/Layanan/Sakip.tsx
@@ -77,7 +77,7 @@ export default function ASakip() {
                           <td className="p-3">{item.judul}</td>
                           <td className="p-3">
                             <a
-                              href={`http://127.0.0.1:8000/storage/${item.file_path}`}
+                              href={`${import.meta.env.VITE_BASE_URL}/storage/${item.file_path}`}
                               className="text-blue-400"
                               download
                             >
