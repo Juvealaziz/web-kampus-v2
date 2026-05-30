6518973cf1760e139a4f5caa32a6f6c06caac33f
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 18:59:08 +0700

    Update Aksi_Prestasi.tsx

--- src/pages/admin/Prestasi/Aksi_Prestasi.tsx
+++ src/pages/admin/Prestasi/Aksi_Prestasi.tsx
@@ -37,7 +37,7 @@ export default function AksiPrestasi() {
         formData.append("gambar", gambar);
       }
 
-      const res = await axios.post("http://127.0.0.1:8000/api/UKM", formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/UKM`, formData);
 
       alert("Berhasil tambah");
       navigate("/admin-prestasi");
