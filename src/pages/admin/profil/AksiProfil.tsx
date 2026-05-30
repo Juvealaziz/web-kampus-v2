419fa8268004bd140d016791c6c8698190cbfda0
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 20:20:28 +0700

    Update AksiProfil.tsx

--- src/pages/admin/profil/AksiProfil.tsx
+++ src/pages/admin/profil/AksiProfil.tsx
@@ -205,7 +205,7 @@ export default function AksiProfil() {
                               "redo",
                             ],
                             ckfinder: {
-                              uploadUrl: "http://127.0.0.1:8000/api/upload",
+                              uploadUrl: `${import.meta.env.VITE_BASE_URL}/api/upload`,
                             },
                           }}
                         />
