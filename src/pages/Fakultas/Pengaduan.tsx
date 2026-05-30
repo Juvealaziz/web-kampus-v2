5cf5e2949adbf5d5e3851b6ce5f1107a82c12779
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 19:38:20 +0700

    Update Pengaduan.tsx

--- src/pages/Fakultas/Pengaduan.tsx
+++ src/pages/Fakultas/Pengaduan.tsx
@@ -35,7 +35,7 @@ export default function Pengaduan() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/pengaduan", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/pengaduan`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
