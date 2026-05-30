45dc4bcde475ab71313a81f9ab115a80c8e3aa71
Muhammad Juve Tariq Al Aziz <154982824+Juvealaziz@users.noreply.github.com>
2026-05-30 19:36:37 +0700

    Update Biaya.tsx

--- src/pages/Fakultas/Biaya.tsx
+++ src/pages/Fakultas/Biaya.tsx
@@ -12,7 +12,7 @@ export default function Biaya() {
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1:8000/api/pages")
+      .get(`${import.meta.env.VITE_API_URL}/pages`)
       .then((res) => {
         const filtered = res.data.filter((item: any) => item.judul === "Biaya");
         setPages(filtered);
